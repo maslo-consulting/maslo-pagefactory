156de98d3ed8800bab1c8673f9d0e6c9cd6f84da
maslo-consulting <rohit@masloconsulting.com>
2026-04-16 12:25:01 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RXZHF809PJ"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RXZHF809PJ');
+        `}}/>
+      </head>
       <body style={{margin:0,fontFamily:"sans-serif",background:"#fff"}}>
         {children}
       </body>
